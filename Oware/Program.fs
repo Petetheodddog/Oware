80c374b969dfaa59e257caab011e022dc8effb40
Luke Wilkinson <lukejamesw@gmail.com>
2020-05-11 11:38:12 +0200

    Added changes

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -39,15 +39,84 @@ let getSeeds n board =
   |12 -> f'
   |_ -> failwith "invalid House"
 
+
+//sets the chosen house to 0 so its seed(s) can be distributed later
+//take in a board and a house number to modify
+let setHouseZero board n =
+  let (a,b,c,d,e,f),(a',b',c',d',e',f') = board.playerNorth.side, board.playerSouth.side
+  match n with
+  |1 -> {board with playerNorth = {board.playerNorth with side = (0,b,c,d,e,f)}}
+  |2 -> {board with playerNorth = {board.playerNorth with side = (a,0,c,d,e,f)}}
+  |3 -> {board with playerNorth = {board.playerNorth with side = (a,b,0,d,e,f)}}
+  |4 -> {board with playerNorth = {board.playerNorth with side = (a,b,c,0,e,f)}}
+  |5 -> {board with playerNorth = {board.playerNorth with side = (a,b,c,d,0,f)}}
+  |6 -> {board with playerNorth = {board.playerNorth with side = (a,b,c,d,e,0)}}
+  //----------------------------Middle of board----------------------------------------
+  |7 -> {board with playerNorth = {board.playerNorth with side = (0,b',c',d',e',f')}}
+  |8 -> {board with playerNorth = {board.playerNorth with side = (a',0,c',d',e',f')}}
+  |9 -> {board with playerNorth = {board.playerNorth with side = (a',b',0,d',e',f')}}
+  |10 -> {board with playerNorth = {board.playerNorth with side = (a',b',c',0,e',f')}}
+  |11 -> {board with playerNorth = {board.playerNorth with side = (a',b',c',d',0,f')}}
+  |12 -> {board with playerNorth = {board.playerNorth with side = (a',b',c',d',e',0)}}
+  |_ -> failwith "Invalid House Number"
+
+
+//Adds a seed to a specified house, will be used l8r
+//takes in a house number to ++ 
+let incrementHouse n (a,b,c,d,e,f,a',b',c',d',e',f') =
+  match n with 
+    |1 -> (a+1,b,c,d,e,f,a',b',c',d',e',f')
+    |2 -> (a,b+1,c,d,e,f,a',b',c',d',e',f')
+    |3 -> (a,b,c+1,d,e,f,a',b',c',d',e',f')
+    |4 -> (a,b,c,d+1,e,f,a',b',c',d',e',f')
+    |5 -> (a,b,c,d,e+1,f,a',b',c',d',e',f')
+    |6 -> (a,b,c,d,e,f+1,a',b',c',d',e',f')
+    //-----------Middle of board-----------
+    |7 -> (a,b,c,d,e,f,a'+1,b',c',d',e',f')
+    |8 -> (a,b,c,d,e,f,a',b'+1,c',d',e',f')
+    |9 -> (a,b,c,d,e,f,a',b',c'+1,d',e',f')
+    |10 -> (a,b,c,d,e,f,a',b',c',d'+1,e',f')
+    |11 -> (a,b,c,d,e,f,a',b',c',d',e'+1,f')
+    |12 -> (a,b,c,d,e,f,a',b',c',d',e',f'+1)
+    |_ -> failwith "Invalid House Number"
+
+
+//Adds the seed(s) from a house to the next houses
+//takes in a house number to ++ 
+let distributeSeeds n (a,b,c,d,e,f,a',b',c',d',e',f') =
+  let rec innerF xs out =
+      match xs with
+      | [] -> out
+      | _::rest -> innerF rest (1::out)
+  innerF xs []
+
+
+
 (*
 useHouse: accepts a House number and a Board, and makes a move using
 that House.
 *)
-let useHouse n board = failwith "Not implemented"
+let useHouse n board = //failwith "Not implemented"
+  let (a,b,c,d,e,f,a',b',c',d',e',f') = board
+  match n with
+  |1 -> 
+  |2 -> 
+  |3 -> 
+  |4 -> 
+  |5 -> 
+  |6 -> 
+  //----------------------------Middle of board----------------------------------------
+  |7 -> 
+  |8 -> 
+  |9 -> 
+  |10 ->
+  |11 ->
+  |12 ->
+  |_ -> failwith "Invalid House Number"
+
 //getSeeds, count the seeds and itt. through them to distribute to Houses greater than the orig, can't use foe's House
 //
-//
-//Psuedo Code
+//          Psuedo Code
 //
 //get the number of seeds from the given house
 //know which players turn it is
@@ -59,7 +128,7 @@ let useHouse n board = failwith "Not implemented"
 //
 // N.B. you CAN'T take seeds from your own side
 //
-//end psuedo code
+//         end psuedo code
 
 (*
 start: accepts a StartingPosition and returns an initialized game where the
@@ -67,10 +136,12 @@ person in the StartingPosition starts the game
 *)
 let start position = failwith "Not implemented"
 
+
 (*
 Score: accepts a Board and gives back a tuple of (southScore , northScore)
 *)
 let score board = failwith "Not implemented"
+
 
 (*
 gameState: accepts a Board and gives back a string that tells us about the
